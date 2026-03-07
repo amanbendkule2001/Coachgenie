8d4dbe6f85cd7717ddd080d0de8176de98907df1
Lokesh-Sohanda8 <lokeshsohanda27@gmail.com>
2026-03-07 20:14:08 +0530

    "Connected DB using Python & altered requirements.txt"

---
+++ PostgreSQL-DB/db-connect.py
@@ -0,0 +1,20 @@
+import psycopg2
+
+conn = psycopg2.connect(
+    database = "postgres",
+    user = "postgres",
+    password = "lokesh123",
+    host = "localhost",
+    port = "5432"
+)
+
+cursor = conn.cursor()
+
+cursor.execute("select * from users")
+
+data = cursor.fetchall()
+
+for d in data:
+    print(d)
+
+cursor.close()
