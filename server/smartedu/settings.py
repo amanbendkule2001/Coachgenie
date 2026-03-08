e0bbcafde401cad6253bc508746cb90c588601c9
Lokesh-Sohanda8 <lokeshsohanda27@gmail.com>
2026-03-08 14:18:02 +0530

    "Initialized apps in the settings.py in Django"

--- server/smartedu/settings.py
+++ server/smartedu/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "auth",
+    'courses',
+    'sessions',
+    'users',
 ]
 
 MIDDLEWARE = [
