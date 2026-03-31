99e22d7cee727251c8706cf19dbec13a5f75ed3b
Lokesh-Sohanda8 <lokeshsohanda27@gmail.com>
2026-03-31 23:36:08 +0530

    "Enquiries: fixed urls.py in Backend"

--- server/smartedu/api/enquiries/urls.py
+++ server/smartedu/api/enquiries/urls.py
@@ -1,5 +1,5 @@
 from rest_framework.routers import DefaultRouter
 from .views import EnquiryViewSet
 router = DefaultRouter()
-router.register(r"enquiries", EnquiryViewSet, basename="enquiry")
+router.register(r"", EnquiryViewSet, basename="enquiry")
 urlpatterns = router.urls
